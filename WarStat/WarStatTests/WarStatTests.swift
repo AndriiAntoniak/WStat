aa88636d9e001ac0f2ac2c1da1c07e8389f56be4
Andrii Antoniak <andrii.antoniak@intellias.com>
2023-11-17 13:30:19 +0200

    Add unit tests target to the app

---
+++ WarStat/WarStatTests/WarStatTests.swift
@@ -0,0 +1,22 @@
+//
+//  WarStatTests.swift
+//  WarStatTests
+//
+//  Created by a.antoniak on 17.11.2023.
+//
+
+import XCTest
+
+final class WarStatTests: XCTestCase {
+    override func setUpWithError() throws {
+        super.setUp()
+    }
+
+    override func tearDownWithError() throws {
+        super.tearDown()
+    }
+
+    func testExample() throws {
+        XCTAssertTrue(true, "This test should always return true")
+    }
+}
